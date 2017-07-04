44518cea658fd199fc9d25df3ef49981a57dc669
keithZmudzinski <kzmudz79@gmail.com>
2017-07-04 12:40:39 -0700

    Uses chrome.downloads.download() now
    No longer updates tab url with download link, instead uses given method. Should solve issue of going to download page instead of just starting the download immediately.

--- kQueue/background.js
+++ kQueue/background.js
@@ -3,6 +3,22 @@ tabID = 0;
 loaded = true;
 
 
+//Creates url objects used in updating tab url
+function makeNextSong(){
+	var url = {
+		url: songQueue[0]
+	}
+	return url;
+}
+//Creates download url object
+function makeDwnObj(url){
+	var obj = {
+		url: url,
+		saveAs: true
+	}
+	return obj;
+}
+//determines which context Menu clicked, acts accordingly
 function onClickHandler(info, tab){
 	tabID = tab.id;
 	if(info.menuItemId == "addItem"){
@@ -11,27 +27,15 @@ function onClickHandler(info, tab){
 		console.log(songQueue[songQueue.length-1] + " added to queue");
 	}
 	else if (info.menuItemId == "downloadItem"){
+		//allows for downloading suggested videos or current video
 		if(info.mediaType == "video"){
-			dwnURL = "https://www.youtubeinmp3.com/fetch/?video="+info.pageUrl;
+			dwnURL = "http://www.youtubeinmp3.com/fetch/?video="+info.pageUrl;
 		}
 		else{
-			dwnURL = "https://www.youtubeinmp3.com/fetch/?video="+info.linkUrl;
+			dwnURL = "http://www.youtubeinmp3.com/fetch/?video="+info.linkUrl;
 		}
-		chrome.tabs.update(tabID, downloadVideo(dwnURL));
+		chrome.downloads.download(makeDwnObj(dwnURL));
 	}
-
-}
-function nextSong(){
-	var url = {
-		url: songQueue[0]
-	}
-	return url;
-}
-function downloadVideo(toDownloadURL){
-	var url = {
-		url: toDownloadURL
-	}
-	return url;
 }
 //Checks if the tab that signals update is
 //	the tab user has queued a video in
@@ -43,7 +47,7 @@ function onUpdated(tabId, changeInfo, tab){
 	 	&& changeInfo.status == "complete"
 		&& loaded){
 			console.log("Song has ended");
-			chrome.tabs.update(tabID, nextSong());
+			chrome.tabs.update(tabID, makeNextSong());
 			songQueue.shift();
 			loaded = false;
 	}// determines when the next tab has finished loading and is playing music
@@ -54,6 +58,7 @@ function onUpdated(tabId, changeInfo, tab){
 		}
 }
 
+//creates the context Menu objects
 var addQItemMenu = chrome.contextMenus.create({"title": "Add to queue", "id": "addItem",
  	"contexts": ["link"], "documentUrlPatterns": ["*://www.youtube.com/*"]});
 var downloadItemMenue = chrome.contextMenus.create({"title": "Download Video", "id": "downloadItem",
